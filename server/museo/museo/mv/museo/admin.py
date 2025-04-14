37cbcc1dad68ef642051b784f7a82c65986221d2
TheFCorti <corti.federic@gmail.com>
2025-04-14 13:35:24 +0200

    admin page fix

--- server/museo/museo/mv/museo/admin.py
+++ server/museo/museo/mv/museo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+admin.register(Utenti)
+admin.register(Categorie)
+admin.register(Opere)
+admin.register(Immagini)
+admin.register(Autori)
+admin.register(Quiz)
+admin.register(Domande)
+admin.register(Risposte)
+admin.register(Partite)

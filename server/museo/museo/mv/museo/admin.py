2c39ace29687ecea5a694c41e3838a12790848bb
TheFCorti <corti.federic@gmail.com>
2025-04-14 13:37:50 +0200

    registrazione modelli

--- server/museo/museo/mv/museo/admin.py
+++ server/museo/museo/mv/museo/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.register(Utenti)
-admin.register(Categorie)
-admin.register(Opere)
-admin.register(Immagini)
-admin.register(Autori)
-admin.register(Quiz)
-admin.register(Domande)
-admin.register(Risposte)
-admin.register(Partite)
+admin.site.register(Utenti)
+admin.site.register(Categorie)
+admin.site.register(Opere)
+admin.site.register(Immagini)
+admin.site.register(Autori)
+admin.site.register(Quiz)
+admin.site.register(Domande)
+admin.site.register(Risposte)
+admin.site.register(Partite)
